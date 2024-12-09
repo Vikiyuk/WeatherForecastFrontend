848917484cffe11bf6f79eeb8f27d6e064cc6cc3
YuriiMaisuradze <maisooradze@gmail.com>
2024-12-09 18:15:15 +0100

    Fixes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherTable from './WeatherTable';
+
+const App = () => {
+  return (
+      <div className="App">
+        <WeatherTable />
+      </div>
+  );
+};
+
+export default App;
